6239b85dc8d2b8aa0b089cb3298ffc067a7d62b2
Bu11vin3 <mitetovalkov@gmail.com>
2026-02-09 23:32:33 +0200

    Added Review Entity Model

---
+++ SternGarage/Models/Review.cs
@@ -0,0 +1,37 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using static MercedesBlog.Common.ValidationConstants;
+
+namespace MercedesBlog.Models
+{
+    public class Review
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Автомобил")]
+        public int CarId { get; set; }
+
+        [ForeignKey("CarId")]
+        public virtual Car? Car { get; set; }
+
+        [Required]
+        [StringLength(ReviewAuthorMaxLength, MinimumLength = ReviewAuthorMinLength)]
+        public string AuthorName { get; set; } = null!;
+
+        [Required]
+        [StringLength(ReviewTitleMaxLength, MinimumLength = ReviewTitleMinLength)]
+        public string Title { get; set; } = null!;
+
+        [Required]
+        [StringLength(ReviewContentMaxLength, MinimumLength = ReviewContentMinLength)]
+        public string Content { get; set; } = null!;
+
+        [Required]
+        [Range(ReviewRatingMin, ReviewRatingMax)]
+        public int Rating { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
