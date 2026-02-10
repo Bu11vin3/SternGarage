f296dadfd8c69879af648667955d8548f443c5e7
Bu11vin3 <mitetovalkov@gmail.com>
2026-02-10 12:43:41 +0200

    Added HomeViewModel

---
+++ SternGarage/ViewModels/HomeViewModel.cs
@@ -0,0 +1,13 @@
+using MercedesBlog.Models;
+
+namespace MercedesBlog.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Car> LatestCars { get; set; } = new List<Car>();
+        public IEnumerable<Review> LatestReviews { get; set; } = new List<Review>();
+        public int TotalCars { get; set; }
+        public int TotalReviews { get; set; }
+        public IEnumerable<CarClass> Classes { get; set; } = new List<CarClass>();
+    }
+}
