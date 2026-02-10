29be47faca555631b6c8b78cc5a68fc67c69abdc
Bu11vin3 <mitetovalkov@gmail.com>
2026-02-10 12:43:33 +0200

    Added CarFormViewModel

---
+++ SternGarage/ViewModels/CarFormViewModel.cs
@@ -0,0 +1,43 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using static MercedesBlog.Common.ValidationConstants;
+
+namespace MercedesBlog.ViewModels
+{
+    public class CarFormViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(CarModelMaxLength, MinimumLength = CarModelMinLength)]
+        public string Model { get; set; } = null!;
+
+        [Required]
+        [Range(CarYearMin, CarYearMax)]
+        public int Year { get; set; }
+
+        [Required]
+        [Display(Name = "Клас")]
+        public int ClassId { get; set; }
+
+        [Required]
+        [Range(CarHorsepowerMin, CarHorsepowerMax)]
+        public int Horsepower { get; set; }
+
+        [Required]
+        [StringLength(CarEngineTypeMaxLength)]
+        public string EngineType { get; set; } = null!;
+
+        [Required]
+        [Range(typeof(decimal), CarPriceMin, CarPriceMax)]
+        public decimal Price { get; set; }
+
+        [StringLength(CarDescriptionMaxLength)]
+        public string? Description { get; set; }
+
+        [Url]
+        public string? ImageUrl { get; set; }
+
+        public IEnumerable<SelectListItem>? Classes { get; set; }
+    }
+}
