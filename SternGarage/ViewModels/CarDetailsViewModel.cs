c5d02b8662f3503a2cbf168ab93ab68f5be5ecb3
Bu11vin3 <mitetovalkov@gmail.com>
2026-02-10 12:42:23 +0200

    Added CarDetailsViewModel

---
+++ SternGarage/ViewModels/CarDetailsViewModel.cs
@@ -0,0 +1,20 @@
+using MercedesBlog.Models;
+
+namespace MercedesBlog.ViewModels
+{
+    public class CarDetailsViewModel
+    {
+        public int Id { get; set; }
+        public string Model { get; set; } = null!;
+        public int Year { get; set; }
+        public string ClassName { get; set; } = null!;
+        public int Horsepower { get; set; }
+        public string EngineType { get; set; } = null!;
+        public decimal Price { get; set; }
+        public string? Description { get; set; }
+        public string? ImageUrl { get; set; }
+        public double AverageRating { get; set; }
+        public int ReviewCount { get; set; }
+        public IEnumerable<Review> Reviews { get; set; } = new List<Review>();
+    }
+}
